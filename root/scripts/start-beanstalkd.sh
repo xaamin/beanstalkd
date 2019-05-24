90dbcdd37065de05dcff7b1e4abd8d47fda247f1
Benjamín Martínez Mateos <benjamin@nulldatata.com>
2019-05-23 21:00:00 -0500

    Custom job size

--- root/scripts/start-beanstalkd.sh
+++ root/scripts/start-beanstalkd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+MAX_JOB_SIZE=${MAX_JOB_SIZE:=1048576}
 
-echo "Running beanstalkd"
+echo "Running beanstalkd /usr/bin/beanstalkd -b /var/lib/beanstalkd/ -z $MAX_JOB_SIZE"
 
-/usr/bin/beanstalkd -b /var/lib/beanstalkd/
+/usr/bin/beanstalkd -b /var/lib/beanstalkd/ -z $MAX_JOB_SIZE
